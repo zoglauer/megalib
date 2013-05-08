91f0a9f3f9719886f67a6e3e8d3a5b8fb4b62694
Andreas Zoglauer <andreas@megalibtoolkit.com>
2013-05-07 19:59:09 -0700

    CHG: Since variables are exported, make them very unique

--- config/env.sh
+++ config/env.sh
@@ -1,39 +1,39 @@
 # Set all MEGAlib-related environment variables
 
 # Store command line
-CMD=""
+MTMP_CMD=""
 while [[ $# -gt 0 ]] ; do
-    CMD="${CMD} $1"
+    MTMP_CMD="${MTMP_CMD} $1"
     shift
 done
 
 
 # Default options:
-NODEFAULT="!@#$%"
-PREFIX=`pwd`
-ROOTPATH=${NODEFAULT}
-GEANT3PATH=${NODEFAULT}
-CLHEPPATH=${NODEFAULT}
-GEANT4PATH=${NODEFAULT}
-HEADASPATH=${NODEFAULT}
-MEGALIBPATH=${NODEFAULT}
+MTMP_NODEFAULT="!@#$%"
+MTMP_PREFIX=`pwd`
+MTMP_ROOTPATH=${MTMP_NODEFAULT}
+MTMP_GEANT3PATH=${MTMP_NODEFAULT}
+MTMP_CLHEPPATH=${MTMP_NODEFAULT}
+MTMP_GEANT4PATH=${MTMP_NODEFAULT}
+MTMP_HEADASPATH=${MTMP_NODEFAULT}
+MTMP_MEGALIBPATH=${MTMP_NODEFAULT}
 
 # Overwrite default options with user options:
-for C in ${CMD}; do
+for C in ${MTMP_CMD}; do
   if [[ ${C} == *-p*=* ]]; then
-    PREFIX=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_PREFIX=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-r*=* ]]; then
-    ROOTPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_ROOTPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-geant3=* ]]; then
-    GEANT3PATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_GEANT3PATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-g*=* ]]; then
-    GEANT4PATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_GEANT4PATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-h*=* ]]; then
-    HEADASPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_HEADASPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-c*=* ]]; then
-    CLHEPPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_CLHEPPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-m*=* ]]; then
-    MEGALIBPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
+    MTMP_MEGALIBPATH=`echo ${C} | awk -F"=" '{ print $2 }'`
   elif [[ ${C} == *-h* ]]; then
     echo ""
     echo "Usage:"
@@ -54,53 +54,53 @@ for C in ${CMD}; do
 done
 
 # Verify path:
-if [[ "${PREFIX}" != */ ]]; then
-  PREFIX="${PREFIX}/"
+if [[ "${MTMP_PREFIX}" != */ ]]; then
+  MTMP_PREFIX="${MTMP_PREFIX}/"
 fi
 
 
 # Set ROOT:
-if [ "${ROOTPATH}" != "${NODEFAULT}" ]; then
-  if [[ "${ROOTPATH}" != /* ]]; then
-    ROOTPATH="${PREFIX}${ROOTPATH}"
+if [ "${MTMP_ROOTPATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_ROOTPATH}" != /* ]]; then
+    MTMP_ROOTPATH="${MTMP_PREFIX}${MTMP_ROOTPATH}"
   fi
 
-  if (test ! -d ${ROOTPATH}); then
+  if (test ! -d ${MTMP_ROOTPATH}); then
     echo ""
-    echo "ERROR: Root directory not found: ${ROOTPATH}"
+    echo "ERROR: Root directory not found: ${MTMP_ROOTPATH}"
     echo ""
     return
   fi
 
-  if (test ! -f ${ROOTPATH}/bin/thisroot.sh); then
+  if (test ! -f ${MTMP_ROOTPATH}/bin/thisroot.sh); then
     echo ""
-    echo "ERROR: Root environment script not found: ${ROOTPATH}/bin/thisroot.sh"
+    echo "ERROR: Root environment script not found: ${MTMP_ROOTPATH}/bin/thisroot.sh"
     echo ""
     return
   fi
    
   # Has to come before HEADAS since both have a libMinuit.so
-  source ${ROOTPATH}/bin/thisroot.sh
+  source ${MTMP_ROOTPATH}/bin/thisroot.sh
 fi
 
 
 # Set CLHEP:
-if [ "${CLHEPPATH}" != "${NODEFAULT}" ]; then
-  if [[ "${CLHEPPATH}" != /* ]]; then
-    CLHEPPATH="${PREFIX}${CLHEPPATH}"
+if [ "${MTMP_CLHEPPATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_CLHEPPATH}" != /* ]]; then
+    MTMP_CLHEPPATH="${MTMP_PREFIX}${MTMP_CLHEPPATH}"
   fi
 
-  if (test ! -d ${CLHEPPATH}); then
+  if (test ! -d ${MTMP_CLHEPPATH}); then
     echo ""
-    echo "ERROR: CLHEP directory not found: ${CLHEPPATH}"
+    echo "ERROR: CLHEP directory not found: ${MTMP_CLHEPPATH}"
     echo ""
     return
   fi
    
-  export CLHEP_BASE_DIR=${CLHEPPATH}
-  export G4CLHEP_BASE_DIR=${CLHEPPATH}
-  export CLHEP_INCLUDE_DIR=${CLHEPPATH}/include
-  export CLHEP_LIB_DIR=${CLHEPPATH}/lib
+  export CLHEP_BASE_DIR=${MTMP_CLHEPPATH}
+  export G4CLHEP_BASE_DIR=${MTMP_CLHEPPATH}
+  export CLHEP_INCLUDE_DIR=${MTMP_CLHEPPATH}/include
+  export CLHEP_LIB_DIR=${MTMP_CLHEPPATH}/lib
   export LD_LIBRARY_PATH=${CLHEP_LIB_DIR}:${LD_LIBRARY_PATH}
   if [[ `uname -a` == *Darwin* ]]; then
     export DYLD_LIBRARY_PATH=${CLHEP_LIB_DIR}/lib:${LD_LIBRARY_PATH}
@@ -109,24 +109,24 @@ fi
 
 
 # Set GEANT4:
-if [ "${GEANT4PATH}" != "${NODEFAULT}" ]; then
-  if [[ "${GEANT4PATH}" != /* ]]; then
-    GEANT4PATH="${PREFIX}${GEANT4PATH}"
+if [ "${MTMP_GEANT4PATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_GEANT4PATH}" != /* ]]; then
+    MTMP_GEANT4PATH="${MTMP_PREFIX}${MTMP_GEANT4PATH}"
   fi
 
-  if (test ! -d ${GEANT4PATH}); then
+  if (test ! -d ${MTMP_GEANT4PATH}); then
     echo ""
-    echo "ERROR: GEANT4 directory not found: ${GEANT4PATH}"
+    echo "ERROR: GEANT4 directory not found: ${MTMP_GEANT4PATH}"
     echo ""
     return
   fi
 
-  if (test -f ${GEANT4PATH}/bin/geant4.sh); then
-    PATHTOGEANT4MAKE=${GEANT4PATH}/share/Geant4-`${GEANT4PATH}/bin/geant4-config --version`/geant4make
-    Here=`pwd`
+  if (test -f ${MTMP_GEANT4PATH}/bin/geant4.sh); then
+    PATHTOGEANT4MAKE=${MTMP_GEANT4PATH}/share/Geant4-`${MTMP_GEANT4PATH}/bin/geant4-config --version`/geant4make
+    MTMP_HERE=`pwd`
     cd ${PATHTOGEANT4MAKE}
     source geant4make.sh
-    cd ${Here}
+    cd ${MTMP_HERE}
 
     if [[ `uname -a` == *Darwin* ]]; then
       export LD_LIBRARY_PATH=${G4LIB}/..:${LD_LIBRARY_PATH}
@@ -134,8 +134,8 @@ if [ "${GEANT4PATH}" != "${NODEFAULT}" ]; then
     fi
 
     export G4NEUTRONHP_USE_ONLY_PHOTONEVAPORATION=1
-  elif (test -f ${GEANT4PATH}/env.sh); then
-    source ${GEANT4PATH}/env.sh > /dev/null  
+  elif (test -f ${MTMP_GEANT4PATH}/env.sh); then
+    source ${MTMP_GEANT4PATH}/env.sh > /dev/null  
 
     export LD_LIBRARY_PATH=${G4INSTALL}/lib/${G4SYSTEM}:${LD_LIBRARY_PATH}
     if [[ `uname -a` == *Darwin* ]]; then
@@ -145,51 +145,51 @@ if [ "${GEANT4PATH}" != "${NODEFAULT}" ]; then
     export G4NEUTRONHP_USE_ONLY_PHOTONEVAPORATION=1
   else
     echo " " 
-    echo "WARNING: geant4.sh or env.sh not found in Geant4 directory: ${GEANT4PATH}/bin"
+    echo "WARNING: geant4.sh or env.sh not found in Geant4 directory: ${MTMP_GEANT4PATH}/bin"
     echo "         Assuming we are currently installing Geant4 otherwise make sure to run \"./Configure\" (Geant4 < 9.5) or \"cmake install\" (Geant4 >= 9.5)!"
     echo ""
-    G4INSTALL=${GEANT4PATH}
+    G4INSTALL=${MTMP_GEANT4PATH}
   fi 
 
 fi
 
 
 # Set GEANT3:
-if [ "${GEANT3PATH}" != "${NODEFAULT}" ]; then
-  if [[ "${GEANT3PATH}" != /* ]]; then
-    GEANT3PATH="${PREFIX}${GEANT3PATH}"
+if [ "${MTMP_GEANT3PATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_GEANT3PATH}" != /* ]]; then
+    MTMP_GEANT3PATH="${MTMP_PREFIX}${MTMP_GEANT3PATH}"
   fi
 
-  if (test ! -d ${GEANT3PATH}); then
+  if (test ! -d ${MTMP_GEANT3PATH}); then
     echo ""
-    echo "ERROR: GEANT3 directory not found: ${GEANT3PATH}"
+    echo "ERROR: GEANT3 directory not found: ${MTMP_GEANT3PATH}"
     echo ""
     return
   fi
    
-  export CERN=${GEANT3PATH}
+  export CERN=${MTMP_GEANT3PATH}
   export CERN_LEVEL=
-  export CERN_ROOT=${GEANT3PATH}
+  export CERN_ROOT=${MTMP_GEANT3PATH}
   export LD_LIBRARY_PATH=${CERN}/lib:${LD_LIBRARY_PATH}
   export PATH=$CERN_ROOT/bin:$PATH   
 fi
 
 
 # Set MEGAlib
-if [ "${MEGALIBPATH}" != "${NODEFAULT}" ]; then
-  if [[ "${MEGALIBPATH}" != /* ]]; then
-    MEGALIBPATH="${PREFIX}${MEGALIBPATH}"
+if [ "${MTMP_MEGALIBPATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_MEGALIBPATH}" != /* ]]; then
+    MTMP_MEGALIBPATH="${MTMP_PREFIX}${MTMP_MEGALIBPATH}"
   fi
 
-  if (test ! -d ${MEGALIBPATH}); then
+  if (test ! -d ${MTMP_MEGALIBPATH}); then
     echo ""
-    echo "ERROR: MEGAlib directory not found: ${MEGALIBPATH}"
+    echo "ERROR: MEGAlib directory not found: ${MTMP_MEGALIBPATH}"
     echo ""
     return
   fi
    
   # Has to come before HEADAS since both have a libMinuit.so
-  export MEGALIB=${MEGALIBPATH}   
+  export MEGALIB=${MTMP_MEGALIBPATH}   
   export PATH=${MEGALIB}/bin:${PATH}    
   export LD_LIBRARY_PATH=${MEGALIB}/lib:${LD_LIBRARY_PATH}
   if [[ `uname -a` == *Darwin* ]]; then
@@ -201,44 +201,44 @@ fi
 
 
 # Set HEADAS:
-if [ "${HEADASPATH}" != "${NODEFAULT}" ]; then
-  if [[ "${HEADASPATH}" != /* ]]; then
-    HEADASPATH="${PREFIX}${HEADASPATH}"
+if [ "${MTMP_HEADASPATH}" != "${MTMP_NODEFAULT}" ]; then
+  if [[ "${MTMP_HEADASPATH}" != /* ]]; then
+    MTMP_HEADASPATH="${MTMP_PREFIX}${MTMP_HEADASPATH}"
   fi
 
-  if (test ! -d ${HEADASPATH}); then
+  if (test ! -d ${MTMP_HEADASPATH}); then
     echo ""
-    echo "ERROR: HEADAS directory not found: ${HEADASPATH}"
+    echo "ERROR: HEADAS directory not found: ${MTMP_HEADASPATH}"
     echo ""
     return
   fi
   
-  HEADASFOUND=false
-  CFITSIOFOUND=false
-  for i in `ls -d ${HEADASPATH}/*86*`; do
+  MTMP_HEADASFOUND=false
+  MTMP_CFITSIOFOUND=false
+  for i in `ls -d ${MTMP_HEADASPATH}/*86*`; do
     if ( test -f ${i}/headas-init.sh ); then 
       export HEADAS=${i}
       alias heainit=". ${HEADAS}/headas-init.sh"
       source ${HEADAS}/headas-init.sh
-      HEADASFOUND=true
+      MTMP_HEADASFOUND=true
     fi
     if [[ `uname -a` == *Linux* ]]; then
       if ( test -f ${i}/lib/libcfitsio.so ); then 
-        CFITSIOFOUND=true
+        MTMP_CFITSIOFOUND=true
       fi
     else
       # Too many installation options here - don't do the check...
-      CFITSIOFOUND=true
+      MTMP_CFITSIOFOUND=true
     fi
   done
 
-  if [ ${HEADASFOUND} == false ]; then
+  if [ ${MTMP_HEADASFOUND} == false ]; then
     echo ""
     echo "ERROR: HEADAS software not found in HEADAS directory"
     echo ""
     return
   fi
-  if [ ${CFITSIOFOUND} == false ]; then
+  if [ ${MTMP_CFITSIOFOUND} == false ]; then
     echo ""
     echo "WARNING: libcfitsio not found in the HEADAS library directory"
     echo "         You should make a link such as libcfitsio_3.XY.so -> libcfitsio.so"
