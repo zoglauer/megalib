46b3e100405da04de37ceb67f9f66faa7b58692e
Andreas Zoglauer <andreas@megalibtoolkit.com>
2014-05-13 19:52:11 -0700

    ADD: Missing files

---
+++ src/spectralyze/inc/MQualifiedIsotope.h
@@ -0,0 +1,87 @@
+/*
+ * MQualifiedIsotope.h
+ *
+ * Copyright (C) by Michelle Galloway & Andreas Zoglauer.
+ * All rights reserved.
+ *
+ * Please see the source-file for the copyright-notice.
+ *
+ */
+
+
+#ifndef __MQualifiedIsotope__
+#define __MQualifiedIsotope__
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Standard libs:
+
+// ROOT libs:
+
+// MEGAlib libs:
+#include "MGlobal.h"
+#include "MIsotope.h"
+
+// Forward declarations:
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! Isotope with additional qualifiying characteristics, such as a rating
+class MQualifiedIsotope : public MIsotope
+{
+  // public interface:
+ public:
+  MQualifiedIsotope();
+  virtual ~MQualifiedIsotope();
+
+  //! Set line found
+  void SetLineFound(unsigned int l, bool Found);
+  //! Get line found
+  bool GetLineFound(unsigned int l);
+
+  //! Set the rating of the isotope
+  void SetRating(double Rating) { m_Rating = Rating; }
+  //! Add something to the rating of the isotope
+  void AddRating(double Rating) { m_Rating += Rating; }  
+  //! Get the rating of the isotope
+  double GetRating() const { return m_Rating; }
+  
+  // protected methods:
+ protected:
+
+  // private methods:
+ private:
+
+
+
+  // protected members:
+ protected:
+
+
+  // private members:
+ private:
+  //! The line branching ratio
+  vector<bool> m_LineFound;
+
+  //! The rating of the isotope
+  double m_Rating;
+ 
+
+#ifdef ___CINT___
+ public:
+  ClassDef(MQualifiedIsotope, 0) // no description
+#endif
+
+};
+
+//! Stream a nucleon
+ostream& operator<<(ostream& os, const MQualifiedIsotope& Isotope);
+
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ src/spectralyze/src/MQualifiedIsotope.cxx
@@ -0,0 +1,114 @@
+/*
+ * MQualifiedIsotope.cxx
+ *
+ *
+ * Copyright (C) by Michelle Galloway & Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Michelle Galloway & Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// MQualifiedIsotope
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Include the header:
+#include "MQualifiedIsotope.h"
+
+// Standard libs:
+
+// ROOT libs:
+
+// MEGAlib libs:
+#include "MStreams.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef ___CINT___
+ClassImp(MQualifiedIsotope)
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! Default constructor
+MQualifiedIsotope::MQualifiedIsotope() : MIsotope()
+{
+  m_Rating = -1;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! Default destructor
+MQualifiedIsotope::~MQualifiedIsotope()
+{
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! Set line found
+void MQualifiedIsotope::SetLineFound(unsigned int l, bool Found)
+{
+  if (l < m_LineFound.size()) {
+    m_LineFound[l] = Found;
+    return;
+  }
+
+  merr<<"Index out of bounds: l="<<l<<" vs. size()="<<m_LineFound.size()<<fatal;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//! Return if the line has been found
+bool MQualifiedIsotope::GetLineFound(unsigned int l)
+{
+  if (l < m_LineFound.size()) return m_LineFound[l];
+
+  merr<<"Index out of bounds: l="<<l<<" vs. size()="<<m_LineFound.size()<<fatal;
+
+  return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+ostream& operator<<(ostream& os, const MQualifiedIsotope& Isotope)
+{
+  os<<"Isotope "<<Isotope.GetName();
+  if (Isotope.GetRating() > 0) {
+    os<<" (rating:"<<Isotope.GetRating()<<")";  
+  }
+  if (Isotope.GetNLines() > 0) {
+    os<<" with the following lines: ";
+    for (unsigned int i = 0; i < Isotope.GetNLines(); ++i) {
+      os<<Isotope.GetLineEnergy(i)<<" keV ("<<Isotope.GetLineBranchingRatio(i)<<")";
+      if (i != Isotope.GetNLines()-1) os<<", ";
+    }  
+  }
+  os<<endl;
+
+  return os;
+}
+
+// MQualifiedIsotope.cxx: the end...
+////////////////////////////////////////////////////////////////////////////////
