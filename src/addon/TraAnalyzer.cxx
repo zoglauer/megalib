5dd7408d3382899744cab0150d238e09cd9f0416
Andreas Zoglauer <andreas@megalibtoolkit.com>
2015-10-08 14:36:53 -0700

    ADD: Example analyzer for tra files

---
+++ src/addon/TraAnalyzer.cxx
@@ -0,0 +1,294 @@
+/* 
+ * TraAnalyzer.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+// Standard
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <csignal>
+#include <map>
+using namespace std;
+
+// ROOT
+#include <TApplication.h>
+#include <TStyle.h>
+#include <TH1.h>
+#include <TCanvas.h>
+#include <MString.h>
+
+// MEGAlib
+#include "MGlobal.h"
+#include "MStreams.h"
+#include "MDGeometryQuest.h"
+#include "MDDetector.h"
+#include "MFileEventsSim.h"
+#include "MDVolumeSequence.h"
+#include "MPhysicalEvent.h"
+#include "MComptonEvent.h"
+#include "MFileEventsTra.h"
+
+/******************************************************************************/
+
+class TraAnalyzer
+{
+public:
+  //! Default constructor
+  TraAnalyzer();
+  //! Default destructor
+  ~TraAnalyzer();
+  
+  //! Parse the command line
+  bool ParseCommandLine(int argc, char** argv);
+  //! Analyze whatever needs to be analyzed...
+  bool Analyze();
+  //! Interrupt the analysis
+  void Interrupt() { m_Interrupt = true; }
+
+private:
+  //! True, if the analysis needs to be interrupted
+  bool m_Interrupt;
+
+  //! Simulation file name
+  MString m_FileName;
+  //! Geometry file name
+  MString m_GeometryFileName;
+};
+
+/******************************************************************************/
+
+
+/******************************************************************************
+ * Default constructor
+ */
+TraAnalyzer::TraAnalyzer() : m_Interrupt(false)
+{
+  gStyle->SetPalette(1, 0);
+  //gROOT->SetBatch(true);
+}
+
+
+/******************************************************************************
+ * Default destructor
+ */
+TraAnalyzer::~TraAnalyzer()
+{
+  // Intentionally left blank
+}
+
+
+/******************************************************************************
+ * Parse the command line
+ */
+bool TraAnalyzer::ParseCommandLine(int argc, char** argv)
+{
+  ostringstream Usage;
+  Usage<<endl;
+  Usage<<"  Usage: TraAnalyzer <options>"<<endl;
+  Usage<<"    General options:"<<endl;
+  Usage<<"         -f:   simulation file name"<<endl;
+  Usage<<"         -o:   output simulation file name"<<endl;
+  Usage<<"         -g:   geometry file name"<<endl;
+  Usage<<"         -h:   print this help"<<endl;
+  Usage<<endl;
+
+  string Option;
+
+  // Check for help
+  for (int i = 1; i < argc; i++) {
+    Option = argv[i];
+    if (Option == "-h" || Option == "--help" || Option == "?" || Option == "-?") {
+      cout<<Usage.str()<<endl;
+      return false;
+    }
+  }
+
+  // Now parse the command line options:
+  for (int i = 1; i < argc; i++) {
+    Option = argv[i];
+
+    // First check if each option has sufficient arguments:
+    // Single argument
+    if (Option == "-f" || Option == "-o") {
+      if (!((argc > i+1) && argv[i+1][0] != '-')){
+        cout<<"Error: Option "<<argv[i][1]<<" needs a second argument!"<<endl;
+        cout<<Usage.str()<<endl;
+        return false;
+      }
+    } 
+    // Multiple arguments_
+    //else if (Option == "-??") {
+    //  if (!((argc > i+2) && argv[i+1][0] != '-' && argv[i+2][0] != '-')){
+    //    cout<<"Error: Option "<<argv[i][1]<<" needs two arguments!"<<endl;
+    //    cout<<Usage.str()<<endl;
+    //    return false;
+    //  }
+    //}
+
+    // Then fulfill the options:
+    if (Option == "-f") {
+      m_FileName = argv[++i];
+      cout<<"Accepting file name: "<<m_FileName<<endl;
+    } else if (Option == "-g") {
+      m_GeometryFileName = argv[++i];
+      cout<<"Accepting file name: "<<m_GeometryFileName<<endl;
+    } else {
+      cout<<"Error: Unknown option \""<<Option<<"\"!"<<endl;
+      cout<<Usage.str()<<endl;
+      return false;
+    }
+  }
+
+  if (m_FileName == "") {
+    cout<<"Error: Need a simulation file name!"<<endl;
+    cout<<Usage.str()<<endl;
+    return false;
+  }
+
+  if (m_GeometryFileName == "") {
+    cout<<"Error: Need a geometry file name!"<<endl;
+    cout<<Usage.str()<<endl;
+    return false;
+  }
+
+  if (m_FileName.EndsWith(".tra") == false) {
+    cout<<"Error: Need a simulation file name, not a "<<m_FileName<<" file "<<endl;
+    cout<<Usage.str()<<endl;
+    return false;
+  }
+
+  return true;
+}
+
+
+/******************************************************************************
+ * Do whatever analysis is necessary
+ */
+bool TraAnalyzer::Analyze()
+{
+  // Load geometry:
+  MDGeometryQuest* Geometry = new MDGeometryQuest();
+  if (Geometry->ScanSetupFile(m_GeometryFileName) == true) {
+    cout<<"Geometry "<<Geometry->GetName()<<" loaded!"<<endl;
+  } else {
+    cout<<"Unable to load geometry "<<Geometry->GetName()<<" - Aborting!"<<endl;
+    return false;
+  }  
+
+  MFileEventsTra* Reader = new MFileEventsTra();
+  if (Reader->Open(m_FileName) == false) {
+    mout<<"Unable to open file "<<m_FileName<<". Aborting!"<<endl;
+  }
+  Reader->ShowProgress();
+
+  TH1D* Hist = new TH1D("Energy of Compton events", "Energy of Compton Events", 100, 0, 1000);
+
+
+  MPhysicalEvent* Event;
+  MComptonEvent* ComptonEvent;
+  while ((Event = Reader->GetNextEvent()) != 0) { 
+    // Hitting Ctrl-C raises this flag
+    if (m_Interrupt == true) return false;
+    
+    if (Event->GetType() == MPhysicalEvent::c_Compton) {
+      ComptonEvent = dynamic_cast<MComptonEvent*>(Event);
+
+      Hist->Fill(ComptonEvent->Ei());
+    }
+
+    delete Event;
+  }
+  
+  TCanvas* Canvas = new TCanvas();
+  Canvas->cd();
+  Hist->Draw();
+  Canvas->Update();
+
+  // Some cleanup
+  delete Reader;
+  delete Geometry;
+
+  
+  return true;
+}
+
+
+/******************************************************************************/
+
+TraAnalyzer* g_Prg = 0;
+int g_NInterrupts = 2;
+
+/******************************************************************************/
+
+
+/******************************************************************************
+ * Called when an interrupt signal is flagged
+ * All catched signals lead to a well defined exit of the program
+ */
+void CatchSignal(int a)
+{
+  cout<<"Catched signal Ctrl-C:"<<endl;
+  
+  --g_NInterrupts;
+  if (g_NInterrupts <= 0) {
+    cout<<"Aborting..."<<endl;
+    abort();
+  } else {
+    cout<<"Trying to cancel the analysis..."<<endl;
+    if (g_Prg != 0) {
+      g_Prg->Interrupt();
+    }
+    cout<<"If you hit "<<g_NInterrupts<<" more times, then I will abort immediately!"<<endl;
+  }
+}
+
+
+/******************************************************************************
+ * Main program
+ */
+int main(int argc, char** argv)
+{
+  // Set a default error handler and catch some signals...
+  signal(SIGINT, CatchSignal);
+
+  // Initialize global MEGAlib variables, especially mgui, etc.
+  MGlobal::Initialize();
+
+  TApplication TraAnalyzerApp("TraAnalyzerApp", 0, 0);
+
+  g_Prg = new TraAnalyzer();
+
+  if (g_Prg->ParseCommandLine(argc, argv) == false) {
+    cerr<<"Error during parsing of command line!"<<endl;
+    return -1;
+  } 
+  if (g_Prg->Analyze() == false) {
+    cerr<<"Error during analysis!"<<endl;
+    return -2;
+  } 
+
+  if (gROOT->IsBatch() == false) {
+    TraAnalyzerApp.Run();
+  }
+
+  cout<<"Program exited normally!"<<endl;
+
+  return 0;
+}
+
+/*
+ * Cosima: the end...
+ ******************************************************************************/
