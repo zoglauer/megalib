5a4e88ae6340258e8cc9727ba4c6a74be42a0654
VasilikiAna <vassia.anagnostatou@gmail.com>
2017-12-12 13:52:59 +0100

    Update MBackprojectionFarField.cxx

--- src/mimrec/src/MBackprojectionFarField.cxx
+++ src/mimrec/src/MBackprojectionFarField.cxx
@@ -319,6 +319,39 @@ bool MBackprojectionFarField::Backproject(MPhysicalEvent* Event, double* Image, 
 ////////////////////////////////////////////////////////////////////////////////
 
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Addition Christian Lang - Backproject
+//---------------------------------------------------------
+bool MBackprojectionSphere::Backproject(MPhysicalEvent* Event, double* Image, int* Bins, int& NUsedBins, double&
+Maximum, double X1Position, double Y1Position, double Z1Position, double X2Position, double Y2Position, double
+Z2Position)
+{
+// Take over all the necessary event data and perform some elementary computations:
+// the compton angle, the cone axis, the most probable origin of the gamma ray
+// if possible, the center of the cone.
+
+if (Assimilate(Event) == false) return false;
+if (Event->GetType() == MPhysicalEvent::c_Compton) {
+return BackprojectionCompton(Image, Bins, NUsedBins, Maximum);
+} else if (Event->GetType() == MPhysicalEvent::c_Pair) {
+return BackprojectionPair(Image, Bins, NUsedBins, Maximum);
+} else if (Event->GetType() == MPhysicalEvent::c_Photo) {
+return BackprojectionPhoto(Image, Bins, NUsedBins, Maximum);
+} else {
+cout<<"Error: Unknown event type for imaging: "<<Event->GetType()<<". Skipping event."<<endl;
+}
+return false;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 bool MBackprojectionFarField::BackprojectionCompton(double* Image, int* Bins, int& NUsedBins, double& Maximum)
 {
   // Compton-Backprojection-algorithm:
@@ -862,235 +895,7 @@ bool MBackprojectionFarField::BackprojectionPhoto(double* Image, int* Bins, int&
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Addition by Christian Lang - BackprojectionComptonLine
-//---------------------------------------------------------
-bool MBackprojectionCartesian::BackprojectionComptonLine(double* Image, int* Bins, int& NUsedBins, double& Maximum,
-double X1Position, double Y1Position, double Z1Position, double X2Position, double Y2Position, double Z2Position
-)
-{
-// Compton-Backprojection-LOR-Intersection-algorithm:
-// The event expands to a double-gausshaped banana and gets intersected with a Line of Response
-//
-// Image
-//: the produced binned image - must have the correct dimensions
-// Limit
-//: the limit
-// NABoveLimit
-//
-Maximum = 0.0;
-NUsedBins = 0;
-// Let the response get the event data:
-m_Response->AnalyzeEvent(m_C); // <- not the correct place...
-int x, y, z;
-double AngleTrans, AngleLong;
-double InnerSum = 0;
-double ConeRadius = 0;
-double Content;
 
-// LOR axis vector
-double ContentLine, Distance;
-double ax = X2Position - X1Position;
-double ay = Y2Position - Y1Position;
-double az = Z2Position - Z1Position;
-double bx, by, bz;
-double Deno = ax*ax+ay*ay+az*az;
-// Cone axis vector (reverse direction of scattered gamma ray)
-double xCA = -m_C->Dg().X();
-double yCA = -m_C->Dg().Y();
-double zCA = -m_C->Dg().Z();
-Rotate(xCA, yCA, zCA);
-// Position of first interaction:
-double xCC = m_C->C1().X();
-double yCC = m_C->C1().Y();
-double zCC = m_C->C1().Z();
-Rotate(xCC, yCC, zCC);
-//cout<<"Cone center: "<<xCC<<":"<<yCC<<":"<<zCC<<endl;
-// The origin of the gamma ray:
-double xOrigin = -m_C->De().X();
-double yOrigin = -m_C->De().Y();
-double zOrigin = -m_C->De().Z();
-Rotate(xOrigin, yOrigin, zOrigin);
-double xBin, yBin, zBin;
-double xn1, yn1, zn1, xn2, yn2, zn2;
-// Important pre-computations
-double Phi = m_C->Phi();
-double tanPhi = tan(Phi);
-//double Threshold = 0.001;
-// Min and max Trans-angle above threshold:
-double Trans_max = m_Response->GetComptonTransversalMax();
-double Trans_min = m_Response->GetComptonTransversalMin();
-double PhiMin = Phi + Trans_min;
-double PhiMax = Phi + Trans_max;
-double cosPhiMin = cos(PhiMin);
-double cosPhiMax = cos(PhiMax);
-
-double L;
-bool HasTrack = m_C->HasTrack();
-
-// ---------> time critical --------->
-// Start the backprojections
-
-unsigned int i;
-unsigned int i_z;
-unsigned int i_yz;
-for (z =0; z < m_x3NBins; z++) {
-i_z = z*m_x1NBins*m_x2NBins;
-for (y = 0; y < m_x2NBins; y++) {
-i_yz = y*m_x1NBins + i_z;
-for (x = 0; x < m_x1NBins; x++) {
-i = x + i_yz; // We fill each x-row
-Content = 0;
-ContentLine = 0;
-xBin = m_xBinCenter[x]-xCC;
-yBin = m_yBinCenter[y]-yCC;
-zBin = m_zBinCenter[z]-zCC;
-AngleTrans = Angle(xBin, yBin, zBin, xCA, yCA, zCA) - Phi;
-
-// tmp: time coincidence only
-////////////////////////////////////////////
-// Skip pixels below threshold:
-
-if (x < m_x1NBins - 1) { // We just do the optimization if there are bins left in x-direction:
-// We now calculate the intersection point between this axis and the cone
-// The cone is defined by (cone center is origin):
-// 0 = (X dot CA)ˆ2 - (X dot X) * (CA dot CA) * sin phi (CA: cone axis)
-// The axis is defined by:
-// X = P + l*(1, 0, 0) where P is the bin center relative to the cone center, i.e. xBin, yBin, zBin
-// Then we determine a, b, c of the Mitternachtsformel and calculate lambda1,2
-// Outside outer ring
-if (AngleTrans > Trans_max + 0.002 || AngleTrans < Trans_min - 0.002) {
-//cout<<m_xBinCenter[x]<<":"<<m_yBinCenter[y]<<endl;
-double CAdotCA = xCA*xCA + yCA*yCA + zCA*zCA; // Per definition "1"!
-double BCdotBC = xBin*xBin + yBin*yBin + zBin*zBin;
-double BCdotCA = xBin*xCA + yBin*yCA + zBin*zCA;
-double cos2PhiRelevant;
-if (AngleTrans > Trans_max) {
-//cout<<"Outside: "<<(AngleTrans - Trans_max)*c_Deg<<endl;
-cos2PhiRelevant = cosPhiMax;
-} else {
-//cout<<"Inside: "<<(AngleTrans - Trans_min)*c_Deg<<endl;
-cos2PhiRelevant = cosPhiMin;
-}
-cos2PhiRelevant *= cos2PhiRelevant;
-double a = xCA*xCA - CAdotCA*cos2PhiRelevant;
-double b = 2*(BCdotCA*xCA - xBin*CAdotCA*cos2PhiRelevant);
-double c = BCdotCA*BCdotCA - BCdotBC*CAdotCA*cos2PhiRelevant;
-double Diskriminante = b*b-4*a*c; // This has to be identical on one x-axis since the distance between
-//axis intersection stays the same
-//cout<<"Diskriminante: "<<Diskriminante<<":"<<a<<":"<<b<<":"<<c<<":"<<CAdotCA<<":"<<BCdotBC<<":"<<BCdotCA
-//<<":"<<cos2PhiRelevant<<endl;
-if (Diskriminante <= 0) { // We ignore the "academic" solution of a tangential point, it’s simply not
-//relevant
-//cout<<"No intersection with cone"<<endl;
-break;
-}
-double Lambda1 = (-b-sqrt(Diskriminante))/(2*a);
-double Lambda2 = (-b+sqrt(Diskriminante))/(2*a);
-// Find the closest solution in x-direction (attention: we are in a coordiante system realtive to cone
-//center!):
-double xNext = 0.0;
-//cout<<"xBC: "<<m_xBinCenter[x]<<" xBin: "<<xBin<<" l1:"<<Lambda1<<" l2:"<<Lambda2<<" dist: "<<Lambda1
-//-Lambda2<<endl;
-if (Lambda1 > 0 && Lambda2 > 0) {
-if (Lambda1 < Lambda2) {
-xNext = Lambda1;
-} else {
-xNext = Lambda2;
-}
-} else if (Lambda1 > 0) {
-xNext = Lambda1;
-} else if (Lambda2 > 0) {
-xNext = Lambda2;
-} else {
-//cout<<"Beyond intersection"<<endl;
-break;
-}
-//cout<<"Chosen lamda: "<<xNext<<endl;
-// Now skip ahead:
-// x+= (int) floor(xNext/m_x1IntervalLength);
-//if (x >= m_x1NBins) break; // Done
-//cout<<"New x: "<<m_xBinCenter[x]<<endl;
-continue;
-}
-}
-
-
-L = sqrt(xBin*xBin + yBin*yBin + zBin*zBin);
-ConeRadius = fabs(tanPhi*Acos(AngleTrans)*L);
-if (HasTrack == true) {
-// angle between two planes spanned by ...
-xn1 = yBin*zCA - zBin*yCA;
-yn1 = (zBin*xCA - xBin*zCA);
-zn1 = xBin*yCA - yBin*xCA;
-xn2 = (yOrigin*zCA - zOrigin*yCA);
-yn2 = (zOrigin*xCA - xOrigin*zCA);
-zn2 = (xOrigin*yCA - yOrigin*xCA);
-AngleLong = Acos((xn1*xn2+yn1*yn2+zn1*zn2)/sqrt((xn1*xn1+yn1*yn1+zn1*zn1)*(xn2*xn2+yn2*yn2+zn2*zn2)));
-//cout<<i<<": "<<ConeRadius<<"!"<<tanPhi<<"!"<<AngleTrans<<"!"<<L<<"-->"<<m_Response->GetResponse(AngleTrans
-//, AngleLong)/ConeRadius<<endl;
-// Sample the 2d-Gauss-function: m_AreaBin[t] is missing ---> new geometry is missing!!!
-Content = m_Response->GetComptonResponse(AngleTrans, AngleLong)/ConeRadius;
-} else {
-Content = m_Response->GetComptonResponse(AngleTrans)/ConeRadius;
-}
-
-
-// calculation of the Line of Response (LOR)
-bx = m_xBinCenter[x] - X1Position;
-by = m_yBinCenter[y] - Y1Position;
-bz = m_zBinCenter[z] - Z1Position;
-Distance = sqrt( ((ay*bz-az*by)*(ay*bz-az*by)
-+
-(az*bx-ax*bz)*(az*bx-ax*bz)
-+
-(ax*by-ay*bx)*(ax*by-ay*bx))
-/ Deno );
-// for simulation only!! (gaussian distribution of a LOR with 0.5 mm FWHM)
-ContentLine = (1/( 0.02128*sqrt(2*c_Pi)) )*exp(-0.5*(Distance/ 0.02128)*(Distance/ 0.02128)); // LOR 0.5mm
-//FWHM
-// Intersection of Compton Cone and LOR
-if (ContentLine > 0.0 && Content > 0.0) {
-//if (ContentLine > 0.0 ) {
-ContentLine = ContentLine * Content; // spatial conicidence
-// search the highest pixel...
-if (ContentLine > Maximum) {Maximum = ContentLine;}
-InnerSum += ContentLine;
-Image[NUsedBins] = ContentLine;
-Bins[NUsedBins] = i;
-++NUsedBins;
-} else {
-
-// Due to acos-rounding error protection, we might get a few here.
-//cout<<"Event "<<m_Event->GetId()<<": I seem to still calculate image bins with no content: x="
-// <<m_xBinCenter[x]<<" y="<<m_yBinCenter[y]<<" z="<<m_yBinCenter[z]<<endl;
-}
-} // end x
-} // end y
-} // end z
-// <--------- time critical <---------
-// If one of the above computed numbers has been NaN
-// InnerSum is also NaN. Then we have to reject this event.
-
-if (InRange(InnerSum) == false) {
-cout<<"Event "<<m_Event->GetId()<<": Catched a NaN!"<<endl;
-cout<<m_C->ToString()<<endl;
-NUsedBins = 0;
-Maximum = 0;
-return false;
-}
-// If the image does not contain any content, return also false
-// This case should not really happen due to protections before, thus the error message
-if (InnerSum == 0 && NUsedBins != 0) {
-cout<<"Event "<<m_Event->GetId()<<": The image seems to be empty..."<<endl;
-NUsedBins = 0;
-Maximum = 0;
-return false;
-}
-return true;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
 
 
 bool MBackprojectionFarField::BackprojectionPair(double* Image, int* Bins, int& NUsedBins, double& Maximum)
