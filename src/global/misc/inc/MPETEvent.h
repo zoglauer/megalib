d9959751e98bb1b7d2ed1f06d33007dde5a45d61
Andreas Zoglauer <andreas@megalibtoolkit.com>
2018-02-19 10:35:41 +0100

    ADD: Uncertainties and time-of-flight

--- src/global/misc/inc/MPETEvent.h
+++ src/global/misc/inc/MPETEvent.h
@@ -44,9 +44,6 @@ class MPETEvent : public MPhysicalEvent
   bool Assimilate(MPETEvent* PETEvent);
   //! Copy the data of the given event into this one
   bool Assimilate(MPhysicalEvent* Event);
-  //! Assimilate the basic event data
-  bool Assimilate(MVector Position1, double Energy1, MVector Position2, double Energy2);
-
   
   //! Stream the data from a file
   virtual bool Stream(MFile& File, int Version, bool Read, bool Fast = false, bool ReadDelayed = false);
@@ -64,20 +61,60 @@ class MPETEvent : public MPhysicalEvent
   //! Get the detected energy of the first event
   double GetEnergy1() const { return m_Energy1; }
   
+  //! Set the detected energy resolution of the first event
+  void SetEnergyResolution1(const double EnergyResolution1) { m_EnergyResolution1 = EnergyResolution1; }
+  //! Get the detected energy resolution of the first event
+  double GetEnergyResolution1() const { return m_EnergyResolution1; }
+  
   //! Set the first detected position of the first event
   void SetPosition1(const MVector& Position1) { m_Position1 = Position1; }
   //! Get the first detected position of the first event
   MVector GetPosition1() const { return m_Position1; }
+  
+  //! Set the first detected position resolution of the first event
+  void SetPositionResolution1(const MVector& PositionResolution1) { m_PositionResolution1 = PositionResolution1; }
+  //! Get the first detected position resolution of the first event
+  MVector GetPositionResolution1() const { return m_PositionResolution1; }
+  
+  //! Set the detection timing of the first event
+  void SetTiming1(const double& Timing1) { m_Timing1 = Timing1; }
+  //! Get the detection timing the first event
+  double GetTiming1() const { return m_Timing1; }
+  
+  //! Set the detection timing resolution of the first event
+  void SetTimingResolution1(const double& TimingResolution1) { m_TimingResolution1 = TimingResolution1; }
+  //! Get the detection timing resolution the first event
+  double GetTimingResolution1() const { return m_TimingResolution1; }
   
   //! Set the detected energy of the second event
   void SetEnergy2(const double Energy2) { m_Energy2 = Energy2; }
   //! Get the detected energy of the second event
   double GetEnergy2() const { return m_Energy2; }
   
+  //! Set the detected energy resolution of the second event
+  void SetEnergyResolution2(const double EnergyResolution2) { m_EnergyResolution2 = EnergyResolution2; }
+  //! Get the detected energy resolution of the second event
+  double GetEnergyResolution2() const { return m_EnergyResolution2; }
+  
   //! Set the first detected position of the second event
   void SetPosition2(const MVector& Position2) { m_Position2 = Position2; }
   //! Get the first detected position of the second event
   MVector GetPosition2() const { return m_Position2; }
+  
+  //! Set the first detected position resolution of the second event
+  void SetPositionResolution2(const MVector& PositionResolution2) { m_PositionResolution2 = PositionResolution2; }
+  //! Get the first detected resolution position of the second event
+  MVector GetPositionResolution2() const { return m_PositionResolution2; }
+  
+  //! Set the detection timing of the second event
+  void SetTiming2(const double& Timing2) { m_Timing2 = Timing2; }
+  //! Get the detection timing the second event
+  double GetTiming2() const { return m_Timing2; }
+  
+  //! Set the detection timing resolution of the second event
+  void SetTimingResolution2(const double& TimingResolution2) { m_TimingResolution2 = TimingResolution2; }
+  //! Get the detection timing resolution the second event
+  double GetTimingResolution2() const { return m_TimingResolution2; }
   
   
   //! Return the total energy
@@ -105,13 +142,30 @@ class MPETEvent : public MPhysicalEvent
  protected:
   //! Detected energy of the first event
   double m_Energy1;
+  //! Detected energy resolution of the first event
+  double m_EnergyResolution1;
   //! First detected position of the first event
   MVector m_Position1;
+  //! First detected position resolution of the first event
+  MVector m_PositionResolution1;
+  //! Detection timing of the first event
+  double m_Timing1;
+  //! Detection timing resolution of the first event
+  double m_TimingResolution1;
+  
   //! Detected energy of the second event
   double m_Energy2;
+  //! Detected energy resolution of the second event
+  double m_EnergyResolution2;
   //! First detected position of the second event
   MVector m_Position2;
-   
+  //! First detected position resolution of the second event
+  MVector m_PositionResolution2;
+  //! Detection timing of the second event
+  double m_Timing2;
+  //! Detection timing resolution of the second event
+  double m_TimingResolution2;
+  
   // private members:
  private:
 
--- src/global/misc/src/MPETEvent.cxx
+++ src/global/misc/src/MPETEvent.cxx
@@ -75,24 +75,30 @@ MPETEvent::~MPETEvent()
 MString MPETEvent::ToString() const
 {
   char Text[1000];
-  MString String("The data of the PET-event:\n"); 
+  MString String("Position-emmision-tomography event:\n"); 
   
-  sprintf(Text, "Energy1: %.3f\n", m_Energy1);
+  sprintf(Text, "  Energy1: %.3f +- %.3f \n", m_Energy1, m_EnergyResolution1);
   String += MString(Text);
   
-  sprintf(Text, "Position1: %.3f, %.3f, %.3f\n", 
-          m_Position1.X(), m_Position1.Y(), 
-          m_Position1.Z());
-  String += MString(Text);
-  
-  sprintf(Text, "Energy2: %.3f\n", m_Energy2);
-  String += MString(Text);
-  
-  sprintf(Text, "Position2: %.3f, %.3f, %.3f\n", 
-          m_Position2.X(), m_Position2.Y(), 
-          m_Position2.Z());
+  sprintf(Text, "  Position1: %.3f, %.3f, %.3f  +- %.3f %.3f  %.3f \n", 
+          m_Position1.X(), m_Position1.Y(), m_Position1.Z(), 
+          m_PositionResolution1.X(), m_PositionResolution1.Y(), m_PositionResolution1.Z());
   String += MString(Text);
 
+  sprintf(Text, "  Timing1: %.9f +- %.3f \n", m_Timing1, m_TimingResolution1);
+  String += MString(Text);
+  
+  sprintf(Text, "  Energy2: %.3f +- %.3f \n", m_Energy2, m_EnergyResolution2);
+  String += MString(Text);
+  
+  sprintf(Text, "  Position2: %.3f, %.3f, %.3f +- %.3f  %.3f  %.3f \n", 
+          m_Position2.X(), m_Position2.Y(), m_Position2.Z(), 
+          m_PositionResolution2.X(), m_PositionResolution2.Y(), m_PositionResolution2.Z());
+  String += MString(Text);
+  
+  sprintf(Text, "  Timing2: %.9f +- %.9f \n", m_Timing2, m_TimingResolution2);
+  String += MString(Text);
+  
 
   return String;
 }
@@ -113,12 +119,21 @@ bool MPETEvent::Assimilate(MPETEvent* PET)
   
   // Get only the basic data and calculate the rest:
   MPhysicalEvent::Assimilate(dynamic_cast<MPhysicalEvent*>(PET));
-
+  
   m_Energy1 = PET->GetEnergy1();       
   m_Position1 = PET->GetPosition1();
+  m_Timing1 = PET->GetTiming1();
   m_Energy2 = PET->GetEnergy2();       
   m_Position2 = PET->GetPosition2();
-
+  m_Timing2 = PET->GetTiming2();
+  
+  m_EnergyResolution1 = PET->GetEnergyResolution1();       
+  m_PositionResolution1 = PET->GetPositionResolution1();
+  m_TimingResolution1 = PET->GetTimingResolution1();
+  m_EnergyResolution2 = PET->GetEnergyResolution2();       
+  m_PositionResolution2 = PET->GetPositionResolution2();
+  m_TimingResolution2 = PET->GetTimingResolution2();
+  
   return Validate();
 }
 
@@ -136,23 +151,6 @@ bool MPETEvent::Assimilate(MPhysicalEvent* Event)
     merr<<"Trying to assimilate a non-PET event!"<<endl; 
     return false; 
   }
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-bool MPETEvent::Assimilate(MVector Position1, double Energy1, MVector Position2, double Energy2)
-{
-  Reset();
-  
-  // Get only the basic data and calculate the rest:
-  m_Energy1 = Energy1;       
-  m_Position1 = Position1;     
-  m_Energy2 = Energy2;       
-  m_Position2 = Position2;     
-  
-  return Validate();
 }
 
 
@@ -178,6 +176,21 @@ bool MPETEvent::Validate()
   if (m_Energy1 <= 0) return false;
   if (m_Energy2 <= 0) return false;
   
+  if (m_EnergyResolution1 < 0) return false;
+  if (m_EnergyResolution2 < 0) return false;
+  
+  if (m_PositionResolution1[0] < 0) return false;
+  if (m_PositionResolution2[0] < 0) return false;
+  
+  if (m_PositionResolution1[1] < 0) return false;
+  if (m_PositionResolution2[1] < 0) return false;
+  
+  if (m_PositionResolution1[2] < 0) return false;
+  if (m_PositionResolution2[2] < 0) return false;
+  
+  if (m_TimingResolution1 < 0) return false;
+  if (m_TimingResolution1 < 0) return false;
+  
   m_IsGoodEvent = true;
 
   return true;
@@ -197,10 +210,12 @@ bool MPETEvent::Stream(MFile& File, int Version, bool Read, bool Fast, bool Read
   if (Read == false) {
     // Write PET specific infos:
     ostringstream S;
-    S<<"EF "<<m_Energy1<<endl;
-    S<<"PF "<<m_Position1[0]<<" "<<m_Position1[1]<<" "<<m_Position1[2]<<endl;
-    S<<"ES "<<m_Energy2<<endl;
-    S<<"PS "<<m_Position2[0]<<" "<<m_Position2[1]<<" "<<m_Position2[2]<<endl;
+    S<<"EF "<<m_Energy1<<" "<<m_EnergyResolution1<<endl;
+    S<<"PF "<<m_Position1[0]<<" "<<m_Position1[1]<<" "<<m_Position1[2]<<"    "<<m_PositionResolution1[0]<<" "<<m_PositionResolution1[1]<<" "<<m_PositionResolution1[2]<<endl;
+    S<<"ZF "<<m_Timing1<<" "<<m_TimingResolution1<<endl;
+    S<<"ES "<<m_Energy2<<" "<<m_EnergyResolution2<<endl;
+    S<<"PS "<<m_Position2[0]<<" "<<m_Position2[1]<<" "<<m_Position2[2]<<"    "<<m_PositionResolution2[0]<<" "<<m_PositionResolution2[1]<<" "<<m_PositionResolution2[2]<<endl;
+    S<<"ZS "<<m_Timing2<<" "<<m_TimingResolution1<<endl;
     File.Write(S);
     File.Flush();
   }
@@ -229,8 +244,9 @@ int MPETEvent::ParseLine(const char* Line, bool Fast)
   if (Line[0] == 'E' && Line[1] == 'F') {
     if (Fast == true) {
       m_Energy1 = strtod(Line+3, NULL);
+      m_EnergyResolution1 = strtod(Line+3, NULL);
     } else {
-      if (sscanf(Line, "EF %lf", &m_Energy1) != 1) {
+      if (sscanf(Line, "EF %lf %lf", &m_Energy1, &m_EnergyResolution1) != 2) {
         cout<<"Unable to parse EF of PET event "<<m_Id<<"!"<<endl;
         Ret = 1;
       }
@@ -240,10 +256,24 @@ int MPETEvent::ParseLine(const char* Line, bool Fast)
       char* p;
       m_Position1[0] = strtod(Line+3, &p);
       m_Position1[1] = strtod(p, &p);
-      m_Position1[2] = strtod(p, NULL);
+      m_Position1[2] = strtod(p, &p);
+      m_PositionResolution1[0] = strtod(p, &p);
+      m_PositionResolution1[1] = strtod(p, &p);
+      m_PositionResolution1[2] = strtod(p, NULL);
     } else {
-      if (sscanf(Line, "PF %lf %lf %lf", &m_Position1[0], &m_Position1[1], &m_Position1[2]) != 3) {
+      if (sscanf(Line, "PF %lf %lf %lf %lf %lf %lf", &m_Position1[0], &m_Position1[1], &m_Position1[2], &m_PositionResolution1[0], &m_PositionResolution1[1], &m_PositionResolution1[2]) != 6) {
         cout<<"Unable to parse PF of PET event "<<m_Id<<"!"<<endl;
+        Ret = 1;
+      }
+    }
+  } else if (Line[0] == 'Z' && Line[1] == 'F') {
+    if (Fast == true) {
+      char* p;
+      m_Timing1 = strtod(Line+3, &p);
+      m_TimingResolution1 = strtod(p, NULL);
+    } else {
+      if (sscanf(Line, "ZF %lf %lf", &m_Timing1, &m_TimingResolution1) != 2) {
+        mout<<"Unable to parse ZF of event "<<m_Id<<"!"<<endl;
         Ret = 1;
       }
     }
@@ -268,6 +298,17 @@ int MPETEvent::ParseLine(const char* Line, bool Fast)
         Ret = 1;
       }
     }
+  } else if (Line[0] == 'Z' && Line[1] == 'S') {
+    if (Fast == true) {
+      char* p;
+      m_Timing2 = strtod(Line+3, &p);
+      m_TimingResolution2 = strtod(p, NULL);
+    } else {
+      if (sscanf(Line, "ZS %lf %lf", &m_Timing2, &m_TimingResolution2) != 2) {
+        mout<<"Unable to parse ZS of event "<<m_Id<<"!"<<endl;
+        Ret = 1;
+      }
+    }
   } else {
     Ret = 2;
   }
@@ -286,9 +327,18 @@ void MPETEvent::Reset()
   MPhysicalEvent::Reset();
   
   m_Energy1 = 0.0;       
-  m_Position1 = MVector(0.0, 0.0, 0.0);     
+  m_Position1 = MVector(0.0, 0.0, 0.0);
+  m_Timing1 = 0;
   m_Energy2 = 0.0;       
-  m_Position2 = MVector(0.0, 0.0, 0.0);     
+  m_Position2 = MVector(0.0, 0.0, 0.0);
+  m_Timing2 = 0;
+  
+  m_EnergyResolution1 = 0.0;       
+  m_PositionResolution1 = MVector(0.0, 0.0, 0.0);
+  m_TimingResolution1 = 0;
+  m_EnergyResolution2 = 0.0;       
+  m_PositionResolution2 = MVector(0.0, 0.0, 0.0);
+  m_TimingResolution2 = 0;
   
   m_IsGoodEvent = false;
 
