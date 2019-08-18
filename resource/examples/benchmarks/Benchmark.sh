a85b5d65a34e46c8a6a2887a4d6e13138bfe764d
Andreas Zoglauer <andreas@megalibtoolkit.com>
2019-08-18 11:10:52 -0700

    CHG: Improved Benchmark

--- resource/examples/benchmarks/Benchmark.sh
+++ resource/examples/benchmarks/Benchmark.sh
@@ -16,7 +16,8 @@ checkload() {
   done
 }
 
-# For compatibility with macOS Catalina, compile a little C program...
+
+# For compatibility with macOS Catalina, compile a little C++ program to get the time in milliseconds
 
 TempEXE=$(mktemp)
 TimerC="${TempEXE}.c"
@@ -37,42 +38,260 @@ _EOF
 g++ ${TimerC} -o ${TempEXE}
 
 
+# Collect information
+OSFlavour=$( cat /etc/os-release | grep ^ID= | awk -F= '{print $2}' )
+OSVersion=$( cat /etc/os-release | grep ^VERSION_ID= | awk -F= '{print $2}' | tr -d '"' )
+Kernel=$( uname -r )
 
-# First run a single thread
+CPUModel=$( lscpu | grep "Model name" | awk -F: '{print $2 }' | sed -e 's/^[[:space:]]*//' )
+CPUSockets=$( lscpu | grep "Socket" | awk -F: '{print $2 }' | sed -e 's/^[[:space:]]*//' )
+CPUCoresPerSocket=$( lscpu | grep "per socket" | awk -F: '{print $2 }' | sed -e 's/^[[:space:]]*//' )
+CPUThreadsPerCore=$( lscpu | grep "per core" | awk -F: '{print $2 }' | sed -e 's/^[[:space:]]*//' )
+Cores=$(( ${CPUSockets} * ${CPUCoresPerSocket} ))
+Threads=$(( ${CPUSockets} * ${CPUCoresPerSocket} * ${CPUThreadsPerCore} ))
+
+Geant4Version=$( geant4-config --version )
+ROOTVersion=$( root-config --version )
+
+MEGAlibVersion=$( cat ${MEGALIB}/config/Version.txt )
+MEGAlibGitHash=$( git log -1 --pretty=format:"%H %cd" --date=short )
+MEGAlibOptimization=$( cat ${MEGALIB}/config/Makefile.options | grep OPT | awk -F"OPT =" '{ print $2 }' | sed -e 's/^[[:space:]]*//' )
+
+# Make nice short CPU name
+CPUName=${CPUModel}
+if [[ ${CPUName} == *Intel* ]]; then
+  CPUName=${CPUName//Intel(R) Xeon(R)}
+  CPUName=${CPUName//Intel(R) Core(TM)}
+  CPUName=${CPUName//CPU}
+  CPUName=$( echo "${CPUName}" | awk -F@ '{ print $1 }' )
+elif [[ ${CPUName} == *Ryzen* ]]; then
+  CPUName=${CPUName//AMD}
+  CPUName=$( echo "${CPUName}" | awk -FCore '{ print $1 }' | awk '{ $NF=""; print}' )
+fi
+CPUName=$( echo -e "${CPUName}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' )
+
+
 checkload
 
-echo "Running single core benchmark"
+CoolDownTime=20
+
+
+
+
+# 1  COSIMA
+
+# First run a single thread
+
+echo " "
+echo "Running single core COSIMA benchmark"
 Start=$( ${TempEXE} )
-cosima -z -v 0 -s 1 -t 0 Benchmark.source > /dev/null
+cosima -z -v 0 -s 1 -f 1 Benchmark.source > /dev/null
 Stop=$( ${TempEXE} )
 
-Duration=$( echo "${Stop} - ${Start}" | bc -l )
-EventsPerSecond=$( echo "100000 / (${Stop} - ${Start})" | bc -l )
-echo "Duration single core: ${Duration} seconds"
-echo "Events per second single core: ${EventsPerSecond} seconds"
+DurationCosimaSingle=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondCosimaSingle=$( echo "scale=3; 100000 / (${Stop} - ${Start})" | bc -l )
+echo "* Duration single core: ${DurationCosimaSingle} seconds"
+echo "* Events per second single core: ${EventsPerSecondCosimaSingle} events/seconds"
 
 
 # Then run all threads:
-checkload
 
-echo "Running multi core benchmark"
-Processes=$( nproc )
+echo "Running multi core COSIMA benchmark"
 Start=$( ${TempEXE} )
-for i in $( seq 1 ${Processes} ); do
-  cosima -z -v 0 -s $i -t $i Benchmark.source > /dev/null &
+for i in $( seq 1 ${Threads} ); do
+  cosima -z -v 0 -s $i -p 1 -f $i Benchmark.source > /dev/null &
 done
 wait
 Stop=$( ${TempEXE} )
 
-Duration=$( echo "${Stop} - ${Start}" | bc -l )
-EventsPerSecond=$( echo "100000 * ${Processes} / (${Stop} - ${Start})" | bc -l )
-echo "Duration multi core: ${Duration} seconds"
-echo "Events per second multi core: ${EventsPerSecond} seconds"
+DurationCosimaMultiple=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondCosimaMultiple=$( echo "scale=3; 100000 * ${Threads} / (${Stop} - ${Start})" | bc -l )
+echo "* Duration multi core: ${DurationCosimaMultiple} seconds"
+echo "* Events per second multi core: ${EventsPerSecondCosimaMultiple} events/seconds"
+
+
+echo " "
+echo "Sleeping for ${CoolDownTime} seconds to let the CPU cool down a bit..."
+sleep ${CoolDownTime}
+
+
+
+
+# 2 REVAN
+
+# First run a single thread
+
+echo " "
+echo "Running single core REVAN benchmark"
+Start=$( ${TempEXE} )
+revan -g Benchmark.geo.setup -f Benchmark.inc1.id1.sim.gz -c Benchmark.revan.cfg -a -n > /dev/null
+Stop=$( ${TempEXE} )
+
+DurationRevanSingle=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondRevanSingle=$( echo "scale=3; 100000 / (${Stop} - ${Start})" | bc -l )
+echo "* Duration single core: ${DurationRevanSingle} seconds"
+echo "* Events per second single core: ${EventsPerSecondRevanSingle} events/seconds"
+
+
+# Then run all threads:
+
+echo "Running multi core REVAN benchmark"
+Start=$( ${TempEXE} )
+for i in $( seq 1 ${Threads} ); do
+  revan -g Benchmark.geo.setup -f Benchmark.p1.inc${i}.id1.sim.gz -c Benchmark.revan.cfg -a -n > /dev/null &
+done
+wait
+Stop=$( ${TempEXE} )
+
+DurationRevanMultiple=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondRevanMultiple=$( echo "scale=3; 100000 * ${Threads} / (${Stop} - ${Start})" | bc -l )
+echo "* Duration multi core: ${DurationRevanMultiple} seconds"
+echo "* Events per second multi core: ${EventsPerSecondRevanMultiple} events/seconds"
+
+
+echo " "
+echo "Sleeping for ${CoolDownTime} seconds to let the CPU cool down a bit..."
+sleep ${CoolDownTime}
+
+
+
+
+# 3 RESPONSECREATOR
+
+# First run a single thread
+
+echo " "
+echo "Running single core RESPONSECREATOR benchmark"
+Start=$( ${TempEXE} )
+responsecreator -m s -o emin=10:emax=600:ebins=600:eskybins=20 -f Benchmark.inc1.id1.sim.gz -g Benchmark.geo.setup -c Benchmark.revan.cfg -b Benchmark.mimrec.cfg -r Benchmark.rsp  > /dev/null
+Stop=$( ${TempEXE} )
+
+DurationResponseCreatorSingle=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondResponseCreatorSingle=$( echo "scale=3; 100000 / (${Stop} - ${Start})" | bc -l )
+echo "* Duration single core: ${DurationResponseCreatorSingle} seconds"
+echo "* Events per second single core: ${EventsPerSecondResponseCreatorSingle} events/seconds"
+
+
+# Then run all threads:
+
+echo "Running multi core RESPONSECREATOR benchmark"
+Start=$( ${TempEXE} )
+for i in $( seq 1 ${Threads} ); do
+  responsecreator -m s -o emin=10:emax=600:ebins=600:eskybins=20 -f Benchmark.p1.inc${i}.id1.sim.gz -g Benchmark.geo.setup -c Benchmark.revan.cfg -b Benchmark.mimrec.cfg -r Benchmark.rsp.${i}  > /dev/null &
+done
+wait
+Stop=$( ${TempEXE} )
+
+DurationResponseCreatorMultiple=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondResponseCreatorMultiple=$( echo "scale=3; 100000 * ${Threads} / (${Stop} - ${Start})" | bc -l )
+echo "* Duration multi core: ${DurationResponseCreatorMultiple} seconds"
+echo "* Events per second multi core: ${EventsPerSecondResponseCreatorMultiple} events/seconds"
+
+
+rm Benchmark.rsp*
+
+
+echo " "
+echo "Sleeping for ${CoolDownTime} seconds to let the CPU cool down a bit..."
+sleep ${CoolDownTime}
+
+
+
+
+# 4 MIMREC imaging
+
+# First run a single thread
+
+echo " "
+echo "Running single core MIMREC imaging benchmark"
+Start=$( ${TempEXE} )
+mimrec -g Benchmark.geo.setup -f Benchmark.inc1.id1.tra.gz -c Benchmark.mimrec.cfg -o Benchmark.jpg -i -n -C MemoryManagement.NThreads=1 > /dev/null
+Stop=$( ${TempEXE} )
+
+DurationMimrecImagingSingle=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondMimrecImagingSingle=$( echo "scale=3; 100000 / (${Stop} - ${Start})" | bc -l )
+echo "* Duration single core: ${DurationMimrecImagingSingle} seconds"
+echo "* Events per second single core: ${EventsPerSecondMimrecImagingSingle} events/seconds"
+
+
+# Then run all threads:
+
+echo "Running multi core MIMREC imaging benchmark"
+Start=$( ${TempEXE} )
+mimrec -g Benchmark.geo.setup -f Benchmark.inc1.id1.tra.gz -c Benchmark.mimrec.cfg -o Benchmark.jpg -i -n -C MemoryManagement.NThreads=${Threads} > /dev/null
+Stop=$( ${TempEXE} )
+
+DurationMimrecImagingMultiple=$( echo "scale=3; ${Stop} - ${Start}" | bc -l )
+EventsPerSecondMimrecImagingMultiple=$( echo "scale=3; 100000 / (${Stop} - ${Start})" | bc -l )
+echo "* Duration multi core: ${DurationMimrecImagingMultiple} seconds"
+echo "* Events per second multi core: ${EventsPerSecondMimrecImagingMultiple} events/seconds"
+
+rm Benchmark*.tra.gz
+rm Benchmark.jpg
+
+
+
+
+# FINALIZE
+
+# Create the summary file
+SummaryFile="${HOSTNAME}_$(date +%Y%m%d_%H%M%S).bm"
+echo "# Benchmark summary file" > ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "HOSTNAME             ${HOSTNAME}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "OSFlavour            ${OSFlavour}" >> ${SummaryFile}
+echo "OSVersion            ${OSVersion}" >> ${SummaryFile}
+echo "Kernel               ${Kernel}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "CPUModel             ${CPUModel}" >> ${SummaryFile}
+echo "CPUName              ${CPUName}" >> ${SummaryFile}
+echo "CPUSockets           ${CPUSockets}" >> ${SummaryFile}
+echo "CPUCoresPerSocket    ${CPUCoresPerSocket}" >> ${SummaryFile}
+echo "CPUThreadsPerCore    ${CPUThreadsPerCore}" >> ${SummaryFile}
+echo "Cores                ${Cores}" >> ${SummaryFile}
+echo "Threads              ${Threads}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "ROOTVersion          ${ROOTVersion}" >> ${SummaryFile}
+echo "Geant4Version        ${Geant4Version}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "MEGAlibVersion       ${MEGAlibVersion}" >> ${SummaryFile}
+echo "MEGAlibGitHash       ${MEGAlibGitHash}" >> ${SummaryFile}
+echo "MEGAlibOptimization  ${MEGAlibOptimization}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "# Cosima" >> ${SummaryFile}
+echo "DurationCosimaSingle           ${DurationCosimaSingle}" >> ${SummaryFile}
+echo "EventsPerSecondCosimaSingle    ${EventsPerSecondCosimaSingle}" >> ${SummaryFile}
+echo "DurationCosimaMultiple         ${DurationCosimaMultiple}" >> ${SummaryFile}
+echo "EventsPerSecondCosimaMultiple  ${EventsPerSecondCosimaMultiple}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "# Revan" >> ${SummaryFile}
+echo "DurationRevanSingle           ${DurationRevanSingle}" >> ${SummaryFile}
+echo "EventsPerSecondRevanSingle    ${EventsPerSecondRevanSingle}" >> ${SummaryFile}
+echo "DurationRevanMultiple         ${DurationRevanMultiple}" >> ${SummaryFile}
+echo "EventsPerSecondRevanMultiple  ${EventsPerSecondRevanMultiple}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "# ResponseCreator" >> ${SummaryFile}
+echo "DurationResponseCreatorSingle           ${DurationResponseCreatorSingle}" >> ${SummaryFile}
+echo "EventsPerSecondResponseCreatorSingle    ${EventsPerSecondResponseCreatorSingle}" >> ${SummaryFile}
+echo "DurationResponseCreatorMultiple         ${DurationResponseCreatorMultiple}" >> ${SummaryFile}
+echo "EventsPerSecondResponseCreatorMultiple  ${EventsPerSecondResponseCreatorMultiple}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+echo "# MimrecImaging" >> ${SummaryFile}
+echo "DurationMimrecImagingSingle           ${DurationMimrecImagingSingle}" >> ${SummaryFile}
+echo "EventsPerSecondMimrecImagingSingle    ${EventsPerSecondMimrecImagingSingle}" >> ${SummaryFile}
+echo "DurationMimrecImagingMultiple         ${DurationMimrecImagingMultiple}" >> ${SummaryFile}
+echo "EventsPerSecondMimrecImagingMultiple  ${EventsPerSecondMimrecImagingMultiple}" >> ${SummaryFile}
+echo " " >> ${SummaryFile}
+
 
 rm Benchmark*.sim.gz
 
 rm ${TempEXE}
 rm ${TimerC}
+
+echo " "
+echo "Done"
 
 exit 0
 
