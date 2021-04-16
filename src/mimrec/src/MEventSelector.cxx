6b7c1b4a9acb8deee7fb6715c6c437770d11ded8
Andreas Zoglauer <andreas@megalibtoolkit.com>
2021-04-15 22:00:02 -0700

    CHG: More error messages if GTI file does not exist

--- src/mimrec/src/MEventSelector.cxx
+++ src/mimrec/src/MEventSelector.cxx
@@ -1536,9 +1536,13 @@ void MEventSelector::SetTimeFile(MString TimeFile)
   m_TimeFile = TimeFile;
   
   m_TimeGTI.Reset(true);
-  
-  if (MFile::Exists(m_TimeFile) == true) {
-    if (m_TimeGTI.Load(m_TimeFile) == false) {
+
+  if (m_TimeMode == 2) {  
+    if (MFile::Exists(m_TimeFile) == true) {
+      if (m_TimeGTI.Load(m_TimeFile) == false) {
+        cout<<"Error: Unable to load GTI file! Using all open one!"<<endl;
+      }
+    } else {
       cout<<"Error: Unable to load GTI file! Using all open one!"<<endl;
     }
   }
