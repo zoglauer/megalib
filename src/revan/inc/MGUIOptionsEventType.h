b9c0bc8680f716a81afc994406c909b3f5a4df79
Adrien Laviron <adrien.laviron@nasa.gov>
2025-06-11 15:03:36 -0400

    ADD: Event type identification algorithm and accompanying GUI options

---
+++ src/revan/inc/MGUIOptionsEventType.h
@@ -0,0 +1,86 @@
+/*
+ * MGUIOptionsEventType.h
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ * Please see the source-file for the copyright-notice.
+ *
+ */
+
+
+#ifndef __MGUIOptionsEventType__
+#define __MGUIOptionsEventType__
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// ROOT libs:
+#include "TGListBox.h"
+
+// MEGAlib libs:
+#include "MGlobal.h"
+#include "MGUIDialog.h"
+#include "MDGeometryQuest.h"
+#include "MSettingsEventReconstruction.h"
+#include "MGUIECBList.h"
+#include "MGUIEFileSelector.h"
+#include "MGUIEEntry.h"
+
+// Forward declarations:
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+class MGUIOptionsEventType : public MGUIDialog
+{
+  // Public Interface:
+ public:
+  MGUIOptionsEventType(const TGWindow* Parent, const TGWindow* Main,
+                      MSettingsEventReconstruction* Data,
+                      MDGeometryQuest* Geometry);
+  virtual ~MGUIOptionsEventType();
+
+
+  // protected methods:
+ protected:
+  virtual void Create();
+  virtual bool OnApply();
+
+  // private methods:
+ private:
+
+  // protected members:
+ protected:
+
+
+  // private members:
+ private:
+  MSettingsEventReconstruction* m_Data;
+  MDGeometryQuest* m_Geometry;
+
+  MGUIECBList* m_CBList;
+  //MGUIEEntry* m_MaxComptonJump;
+  //MGUIEEntry* m_NSequencesToKeep;
+  //TGCheckButton* m_RejectPurelyAmbiguousSequences;
+  MGUIEFileSelector* m_FileSelector;
+  MGUIEEntry* m_NLayersForVertexSearch;
+  //MGUIEEntry* m_SigmaHitPos;
+
+  TGListBox* m_DetectorList;
+
+  enum ButtonCodes { e_DoTracking = 100, e_RejectPurelyAmbiguousSequences, e_Detectors };
+
+#ifdef ___CLING___
+ public:
+  ClassDef(MGUIOptionsEventType, 0)
+#endif
+
+};
+
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
