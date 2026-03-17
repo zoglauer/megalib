da4d1d99109b3fa298cfb642d626e771f985402b
Isabella Sanford <isabellalynn622@gmail.com>
2026-03-17 10:08:31 -0400

    added print statement to esure code runs AND added printing for event ID

--- src/revan/src/MERTrackFirstTwoLayers.cxx
+++ src/revan/src/MERTrackFirstTwoLayers.cxx
@@ -21,9 +21,24 @@ ClassImp(MERTrackFirstTwoLayers)
 MERTrackFirstTwoLayers::MERTrackFirstTwoLayers() : MERTrack()
 {
   // Construct an instance of MERTrackFirstTwoLayers
+  // printing a statement to make sure it runs (it does!)
+  cout << "Running new tracker" << endl;
 }
 
 MERTrackFirstTwoLayers::~MERTrackFirstTwoLayers()
 {
   // Delete this instance of MERTrackFirstTwoLayers
 }   
+
+// after creating the instance of MERTrackFirstTwoLayers, this method tracks the pairs (using existing function)
+// just here to make sure that I can access the event ID and print it
+
+// calls the TrackPairs method which is used in the MERTrack class -> MUST CHANGE FOR MY ACTUAL RECONSTRUCTION
+void MERTrackFirstTwoLayers::TrackPairs(MRERawEvent* RE)
+{
+  cout << "Event " << RE->GetEventID() << endl;
+
+  MERTrack::TrackPairs(RE);
+}
+
+// THIS WORKS!
