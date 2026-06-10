d752693d57195ae4c1912ce1c64adfeb237fef5e
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-06-10 16:45:17 +0200

    Fix GetSecondaryParticleID to GetParticleID

--- src/revan/src/MFileEventsEvta.cxx
+++ src/revan/src/MFileEventsEvta.cxx
@@ -494,7 +494,7 @@ MRERawEvent* MFileEventsEvta::GetNextEventBinary()
           Event->AddREAM(GR);
         }
         if (m_SaveOI == true && SimEvent->GetNIAs() > 0) {
-          Event->SetOriginInformation(SimEvent->GetIAAt(0)->GetPosition(), SimEvent->GetIAAt(0)->GetSecondaryDirection(), SimEvent->GetIAAt(0)->GetSecondaryPolarization(), SimEvent->GetIAAt(0)->GetSecondaryEnergy(),SimEvent->GetIAAt(0)->GetSecondaryParticleID());
+          Event->SetOriginInformation(SimEvent->GetIAAt(0)->GetPosition(), SimEvent->GetIAAt(0)->GetSecondaryDirection(), SimEvent->GetIAAt(0)->GetSecondaryPolarization(), SimEvent->GetIAAt(0)->GetSecondaryEnergy(),SimEvent->GetIAAt(0)->GetParticleID());
         }
         
         delete SimEvent;
